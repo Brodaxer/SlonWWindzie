0e4bc7ffb3db49e2337735db82cf4b68c9929048
Brodaxer <115712630+Brodaxer@users.noreply.github.com>
2023-01-31 12:25:09 +0100

    Add files via upload

---
+++ Lift.java
@@ -0,0 +1,21 @@
+class Lift {
+    private Person[] people = new Person[4];
+    private int peopleNumber = 0;
+    private final int maxWeight = 400;
+
+    void add(Person person) {
+
+    }
+
+    void start() {
+
+    }
+
+    int getTotalWeight() {
+
+    }
+
+    void clear() {
+
+    }
+}
