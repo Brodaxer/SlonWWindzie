99361b0305e31db568a5f1c9720a4203d8424dd1
Brodaxer <115712630+Brodaxer@users.noreply.github.com>
2023-02-01 13:40:55 +0100

    Add files via upload

--- Lift.java
+++ Lift.java
@@ -3,31 +3,36 @@ class Lift {
     private int peopleNumber = 0;
     private final int maxWeight = 400;
 
-    public Person[] getPeople() {
-        return people;
-    }
-
-    public void setPeople(Person[] people) {
-        this.people = people;
-    }
 
     void add(Person person) {
-        if (peopleNumber <= 4) {
-
+        if (peopleNumber == 0) {
             people[peopleNumber] = new Person(person.getId(), person.getWeight());
-
             peopleNumber++;
             people[peopleNumber] = new Person();
+            people[peopleNumber + 1] = new Person();
+            people[peopleNumber + 2] = new Person();
+        } else if (peopleNumber <= 1) {
+            people[peopleNumber] = new Person(person.getId(), person.getWeight());
+            peopleNumber++;
+            people[2] = new Person();
+            people[3] = new Person();
+        } else if (peopleNumber <= 2) {
+            people[peopleNumber] = new Person(person.getId(), person.getWeight());
+            peopleNumber++;
+            people[peopleNumber] = new Person();
+        } else if (peopleNumber < 4) {
+            people[peopleNumber] = new Person(person.getId(), person.getWeight());
+            peopleNumber++;
+        } else System.out.println("Przekroczono limit miejsc");
 
-        } else {
-            System.out.println("Przekroczono limit miejsc");
-        }
 
     }
 
-    void start(Person[] p) {
-        if (getTotalWeight(p) > maxWeight) {
-            System.out.println("Winda przeciazona (" + (getTotalWeight(p) - maxWeight) + "kg ponad limit)");
+    void start() {
+        if (peopleNumber >4 ) {
+            System.out.println("Przekroczono limit miejsc");
+        } else if (getTotalWeight() > maxWeight) {
+            System.out.println("Winda przeciazona (" + (getTotalWeight() - maxWeight) + "kg ponad limit)");
 
         } else {
             System.out.println("Winda ruszyla");
@@ -36,12 +41,15 @@ class Lift {
 
     }
 
-    int getTotalWeight(Person[] p) {
-        return p[0].getWeight() + p[1].getWeight() + p[2].getWeight() + p[3].getWeight();
+    int getTotalWeight() {
+        return people[0].getWeight() + people[1].getWeight() + people[2].getWeight() + people[3].getWeight();
     }
 
     void clear() {
         peopleNumber = 0;
         people[peopleNumber] = new Person();
+        people[peopleNumber + 1] = new Person();
+        people[peopleNumber + 2] = new Person();
+        people[peopleNumber + 3] = new Person();
     }
 }
--- liftSimulation.java
+++ liftSimulation.java
@@ -1,25 +1,27 @@
 public class liftSimulation {
     public static void main(String[] args) {
         Person person1 = new Person(1, 100);
-        Person person2 = new Person(2, 100);
+        Person person2 = new Person(2, 80);
         Person person3 = new Person(3, 90);
         Person person4 = new Person(4, 120);
-        Person person5 = new Person(5, 1);
+        Person person5 = new Person(5, 110);
 
         Lift lift = new Lift();
         lift.add(person1);
         lift.add(person2);
         lift.add(person3);
-//        lift.add(person4);
-//        lift.add(person5);
-        lift.getTotalWeight(lift.getPeople());
-        System.out.println(lift.getTotalWeight(lift.getPeople()));
-        lift.start(lift.getPeople());
+        lift.add(person4);
+        lift.add(person5);
+        lift.getTotalWeight();
+        System.out.println(lift.getTotalWeight());
+        lift.start();
+
         lift.clear();
         lift.add(person5);
-        lift.getTotalWeight(lift.getPeople());
-        lift.start(lift.getPeople());
-        System.out.println(lift.getTotalWeight(lift.getPeople()));
+        lift.add(person1);
+        lift.getTotalWeight();
+        lift.start();
+        System.out.println(lift.getTotalWeight());
         // jeszcze nie skonczylem ale na dzisiaj tak xd
     }
 }
